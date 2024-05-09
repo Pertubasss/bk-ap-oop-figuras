d1506a5677b0b96d579c96148bf49a72551770ea
Pertubasss <henriquessmesquita@gmail.com>
2024-05-09 18:42:52 -0300

    Resolução tipo de triangulo utilizando OOP

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Triangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(">>> Tipo do triângulo <<<");
+
+            double[] lados = LerLados();
+
+            var triangulo = new Triangulo(lados[0], lados[1], lados[2]);
+            var tipo = triangulo.TipoTriangulo();
+
+            Console.WriteLine($"O triângulo é {tipo}.");
+
+        }
+
+        static double[] LerLados()
+        {
+            double[] ladosTriangulo = new double[3];
+
+            Console.WriteLine("Digite o comprimento dos lados do triângulo");
+
+            for (int i = 0; i < 3; i++)
+            {
+                bool numValido = false;
+
+                while (!numValido)
+                {
+                    Console.Write($"Lado {i + 1}: ");
+                    numValido = double.TryParse(Console.ReadLine(), out ladosTriangulo[i]) && ladosTriangulo[i] > 0;
+
+                    if (!numValido)
+                        Console.WriteLine("Valor inválido. Digite um número válido.");
+                }
+            }
+
+            return ladosTriangulo;
+        }
+    }
+}
---
+++ Triangulo/Triangulo/Triangulo.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Triangulo
+{
+    public class Triangulo(double lado1, double lado2, double lado3)
+    {
+        public double Lado1 { get; set; } = lado1;
+        public double Lado2 { get; set; } = lado2;
+        public double Lado3 { get; set; } = lado3;
+
+        public bool VerificarTriangulo()
+        {
+            return Lado1 + Lado2 > Lado3 
+                && Lado1 + Lado3 > Lado2 
+                && Lado2 + Lado3 > Lado1;
+        }
+
+        public string TipoTriangulo()
+        {
+            if (!VerificarTriangulo())
+                return "Não é um triangulo!";
+
+            if (Lado1 == Lado2 && Lado2 == Lado3)
+            {
+                return "Equilátero";
+            }
+
+            else if (Lado1 == Lado2 || Lado1 == Lado3 || Lado2 == Lado3)
+            {
+                return "Isósceles";
+            }
+
+            else
+            {
+                return "Escaleno";
+            }
+        }
+    }
+}
